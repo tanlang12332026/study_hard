f6a54caddf1374096415fc1a87623b3eaa1ac10a
tanlang12332026 <tanlang1233@gmail.com>
2026-01-19 22:19:16 +0800

    dsds

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ async def _handleSearchKVM(q:str, cache: bool):
     print(f'耗时 ---{end_time}')
     if len(urls) > 0:
         if redis:
-            await redis.setex(cache_url, 3600 * 6, json.dumps(urls))
+            await redis.setex(cache_url, 3600 * 3, json.dumps(urls))
             await redis.aclose()
 
         if redis:
